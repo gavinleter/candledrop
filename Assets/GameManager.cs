d4d9a8aa9f89d3ae6f5e89b89a8e1c739aff370e
Gavin Lester <gavinlester@arizona.edu>
2023-09-27 01:16:32 -0700

    candlegame copy(8)

---
+++ Assets/GameManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] canObjects = new GameObject[12];
+    public Transform teleCoords;
+    public float minTurnDuration = 2.0f; // Minimum turn duration in seconds
+    public float velocityCheckDelay = 0.05f; // Delay before checking velocity
+
+    private GameObject selectedCan;
+    private bool isTurnActive = false;
+    private bool canMove = false;
+    private float turnStartTime;
+    private float lastMoveTime;
+    private bool hasMoved = false;
+
+    private void Start()
+    {
+        StartTurn();
+    }
+
+    private void Update()
+    {
+        if (isTurnActive)
+        {
+            Rigidbody2D rb = selectedCan.GetComponent<Rigidbody2D>();
+
+            if (canMove && Time.time - lastMoveTime >= velocityCheckDelay && rb.velocity.magnitude < 0.01f)
+            {
+                // Object has stopped moving, start a new turn
+                StartTurn();
+            }
+            else if (Time.time - turnStartTime >= minTurnDuration && Input.GetMouseButtonDown(0) && !hasMoved)
+            {
+                Vector3 tapPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                tapPosition.z = 0;
+
+                if (selectedCan != null)
+                {
+                    selectedCan.transform.position = new Vector3(teleCoords.position.x, teleCoords.position.y, 0);
+
+                    rb.gravityScale = 1;
+                    rb.isKinematic = false;
+
+                    Vector3 newPosition = new Vector3(tapPosition.x, selectedCan.transform.position.y, 0);
+                    selectedCan.transform.position = newPosition;
+
+                    canMove = true;
+                    lastMoveTime = Time.time + velocityCheckDelay; // Delay before checking velocity
+                    hasMoved = true; // Candle has been moved
+                }
+            }
+        }
+    }
+
+    private void StartTurn()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, canObjects.Length); // Specify UnityEngine.Random
+        selectedCan = Instantiate(canObjects[randomIndex], teleCoords.position, Quaternion.identity);
+        selectedCan.SetActive(true);
+
+        Rigidbody2D rb = selectedCan.GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
+
+        canMove = false;
+        isTurnActive = true;
+        turnStartTime = Time.time;
+        hasMoved = false; // Reset the moved flag
+    }
+}
