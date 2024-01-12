f4371f0e7d498649b2bf5b539827a99fef3b992b
Gavin Lester <gavinlester@arizona.edu>
2024-01-12 01:14:58 -0700

    major basement update, locks and buttons

---
+++ Assets/corrupted_candledrop_text_file.py
@@ -0,0 +1,146 @@
+Candledrop main text file
+---------------------------------------------------
+
+Achievements unlock status:
+
+
+I_Know_How_To_Play == False
+
+Deaf == False
+
+Catastrophe == False
+
+Afraid_Of_Heights == False
+#unlocking this achievement should
+#unlock the "N" starting candle 
+
+Minimalist == False
+
+Maximalist == False
+
+God_Of_Maximalism == False
+#unlocking this achievement should
+#unlock the "flare" starting candle
+
+Failure_Speedrun == False
+
+The_Long_Haul == False
+
+Poopy_Doopy == False
+#unlocking this achievement should
+#unlock the "fat" starting candle
+
+Not_Too_Shabby == False
+
+Pretty_Good == False
+
+Impressive == False
+#unlocking this achievement should
+#unlock the "bronze" skin option
+
+Insanely_High == False
+#unlocking this achievement should
+#unlock the "silver" skin option
+
+Unbelievable == False
+#unlocking this achievement should
+#unlock the "gold" skin option
+
+Impossible == False
+
+Only_Up_From_Here == False
+
+Major_Improvement == False
+
+Color_Conga_Line == False
+
+Dichromic == False
+
+Monochromic == False
+
+ADvantage == False
+
+ADvanced_Tactics == False
+
+I_Love_You == False
+
+I_Get_To_Eat_Tonight == False
+
+College_Student_Debt_Fund == False
+
+Secret_Button_Hunter == False
+#unlocking this achievement should
+#unlock the "alt_music" setting, which
+#toggles alternate main game music
+
+Babys_First_Combo == False
+
+Combo_God == False
+
+Candle_MegaFan == False
+
+Skin_Diff == False
+
+Kit_Diff == False
+
+Dont_Tap_It == False
+
+Vased == False
+
+Waxicide == False
+
+Friendly_Fire == False
+
+Wafflin_About == False
+#unlocking this achievement should
+#unlock the "waffle" setting, which
+#toggles waffle rain
+
+------------------------------------------
+Major Unlocks:
+
+Achievement_Counter == 0
+
+Current_Score == 0
+
+Highscore == 0
+
+Fat == False
+#unlock this starting candle
+# when "Poopy_Doopy" is True
+
+Candleabra == False
+#unlock this starting candle
+# when 20 achievements are unlocked
+
+N == False
+#unlock this starting candle
+# when "Afraid_Of_Heights" is True
+
+Flare == False
+#unlock this starting candle
+# when "God_Of_Maximalism" is True
+
+Waffle == False
+#unlock this settings button
+# when "Wafflin'_About" is True
+
+Alt_Song == False
+#unlock this settings button
+# when "Secret_Button_Hunter" is True
+
+Bronze_Skin == False
+#unlock this starting candle skin
+#when "Impressive" is True
+
+Silver_Skin == False
+#unlock this starting candle skin
+#when "Insanely_High" is True
+
+Gold_Skin == False
+#unlock this starting candle skin
+#when "Unbelieveable" is True
+
+Glass_Skin == False
+#unlock this starting candle skin
+#when "Dont_Tap_It" is True
