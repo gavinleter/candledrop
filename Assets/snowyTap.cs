ffb50bbcd4811e78045d5771ce7a6df3a8275989
Gavin Lester <gavinlester@arizona.edu>
2024-04-18 23:38:03 -0700

    make snowy chirp!

---
+++ Assets/snowyTap.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snowyTap : MonoBehaviour
+{
+
+    [SerializeField] AudioClip chirp1;
+    [SerializeField] AudioClip chirp2;
+    [SerializeField] AudioClip chirp3;
+
+    AudioSource audioSourceChirp1;
+    AudioSource audioSourceChirp2;
+    AudioSource audioSourceChirp3;
+
+
+    private List<System.Action> actions;
+
+    public bool active = true;
+
+    virtual protected void Start(){
+
+        audioSourceChirp1 = gameObject.AddComponent<AudioSource>();
+        audioSourceChirp2 = gameObject.AddComponent<AudioSource>();
+        audioSourceChirp3 = gameObject.AddComponent<AudioSource>();
+
+        audioSourceChirp1.clip = chirp1;
+        audioSourceChirp2.clip = chirp2;
+        audioSourceChirp3.clip = chirp3;
+
+        actions = new List<System.Action>();
+    }
+
+    virtual protected void OnMouseDown(){
+        if (active) {
+
+            if (Settings.soundEnabled) 
+            {
+                int chirpSound = Random.Range(0,2);
+
+                
+                if (chirpSound == 0) {
+
+                    audioSourceChirp1.Play();
+                }
+                else if (chirpSound == 1) {
+
+                    audioSourceChirp2.Play();
+                }
+                else if (chirpSound == 2) {
+
+                    audioSourceChirp3.Play();
+                }
+            }
+
+        }
+    }
+}
