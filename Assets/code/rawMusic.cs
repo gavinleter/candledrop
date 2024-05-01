ad1394e6916879f17089d09ef0d2d6692837d29c
Gavin Lester <gavinlester@arizona.edu>
2024-04-30 17:53:02 -0700

    major skin stuff update, starting music wish me luck!

---
+++ Assets/code/rawMusic.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rawMusic : MonoBehaviour
+{
+    [SerializeField] AudioClip credits;
+    [SerializeField] AudioClip space;
+
+    [SerializeField] AudioClip MainMenu;
+
+    [SerializeField] AudioClip MainGame;
+
+    [SerializeField] AudioClip Basement;
+
+    [SerializeField] AudioClip GameOver;
+
+    [SerializeField] AudioClip AlternateGame;
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
