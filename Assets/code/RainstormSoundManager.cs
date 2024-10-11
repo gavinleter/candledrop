e8431626a2d72e1a2046a4f8a2f9337125126ef3
Gavin C Lester <gavinclester@gmail.com>
2024-10-10 21:03:25 -0700

    jimmy secret plus some layering tweaks

--- Assets/code/RainstormSoundManager.cs
+++ Assets/code/RainstormSoundManager.cs
@@ -3,19 +3,27 @@ using UnityEngine;
 public class RainstormSoundManager : MonoBehaviour
 {
     public AudioClip rainstormSound;
+    public AudioClip insideRainstormSound;
     public Transform mainCamera;
     public float volumeTransitionSpeed = 3.0f;
 
-    private AudioSource audioSource;
+    private AudioSource audioSource1;
+    private AudioSource audioSource2;
     
     private void Start()
     {
         
-        audioSource = gameObject.AddComponent<AudioSource>();
-        audioSource.clip = rainstormSound;
-        audioSource.loop = true;
-        audioSource.Play();
-        audioSource.volume = 0f;
+        audioSource1 = gameObject.AddComponent<AudioSource>();
+        audioSource1.clip = rainstormSound;
+        audioSource1.loop = true;
+        audioSource1.Play();
+        audioSource1.volume = 0f;
+
+        audioSource2 = gameObject.AddComponent<AudioSource>();
+        audioSource2.clip = insideRainstormSound;
+        audioSource2.loop = true;
+        audioSource2.Play();
+        audioSource2.volume = 0f;
     }
 
 
@@ -25,26 +33,31 @@ public class RainstormSoundManager : MonoBehaviour
         if (mainCamera.position.y > 70.0)
         {
 
-                audioSource.volume = Mathf.Lerp(audioSource.volume, 0.0f, Time.deltaTime * volumeTransitionSpeed);
+                audioSource1.volume = Mathf.Lerp(audioSource1.volume, 0.0f, Time.deltaTime * volumeTransitionSpeed);
+                audioSource2.volume = Mathf.Lerp(audioSource2.volume, 0.0f, Time.deltaTime * volumeTransitionSpeed);
+
         }
 
 
         else if (mainCamera.position.y > 45.0 && mainCamera.position.y < 70.0)
         {
     
-                audioSource.volume = Mathf.Lerp(audioSource.volume, 0.7f, Time.deltaTime * volumeTransitionSpeed * 0.05f);
+                audioSource1.volume = Mathf.Lerp(audioSource1.volume, 0.7f, Time.deltaTime * volumeTransitionSpeed * 0.05f);
+                audioSource2.volume = Mathf.Lerp(audioSource2.volume, 0.0f, Time.deltaTime * volumeTransitionSpeed * 0.05f);
         }
 
 
         else if (mainCamera.position.y > 0.0 && mainCamera.position.y < 45.0)
         {
-                // Camera is above 65, set volume to 0% instantly
-                audioSource.volume = Mathf.Lerp(audioSource.volume, 0.05f, Time.deltaTime * volumeTransitionSpeed * 0.2f);
+        
+                audioSource1.volume = Mathf.Lerp(audioSource1.volume, 0.05f, Time.deltaTime * volumeTransitionSpeed * 0.2f);
+                audioSource2.volume = Mathf.Lerp(audioSource2.volume, 0.3f, Time.deltaTime * volumeTransitionSpeed * 0.2f);
         }
 
         else
         {
-                audioSource.volume = Mathf.Lerp(audioSource.volume, 0.0f, Time.deltaTime * volumeTransitionSpeed);
+                audioSource1.volume = Mathf.Lerp(audioSource1.volume, 0.0f, Time.deltaTime * volumeTransitionSpeed);
+                audioSource2.volume = Mathf.Lerp(audioSource2.volume, 0.0f, Time.deltaTime * volumeTransitionSpeed);
 
         }
     }
--- Assets/code/WaffleButton.cs
+++ Assets/code/WaffleButton.cs
@@ -8,25 +8,52 @@ public class WaffleButton : ButtonPress {
     [SerializeField] Sprite[] waffleSprites;
     [SerializeField] AudioClip[] waffleBiteSounds;
 
+    [SerializeField] AudioClip jimmyCube;
+
     ParticleSystem[] waffleBiteParticles;
+
+    [SerializeField] private ParticleSystem jimmyRain;
+    private AudioSource jimmySourceDown;
     int biteCount = 0;
 
     protected override void Start() {
         base.Start();
 
         waffleBiteParticles = GetComponentsInChildren<ParticleSystem>();
-
+        jimmySourceDown =  gameObject.AddComponent<AudioSource>();
+        jimmySourceDown.clip = jimmyCube;
         audioSourceDown.volume = 1;
+        jimmySourceDown.volume = 0;
     }
 
 
     protected override void MouseDown() {
-
-        if (biteCount < waffleSprites.Length) {
-
-            audioSourceDown.clip = waffleBiteSounds[biteCount];
+        
+        if (biteCount < 3) {
+            
+            audioSourceDown.clip = waffleBiteSounds[0];
             GetComponent<SpriteRenderer>().sprite = waffleSprites[biteCount];
             waffleBiteParticles[biteCount].Play();
+            biteCount++;
+
+        }
+        else if(biteCount == 11){
+
+            return;
+        }
+
+
+        else if(biteCount == 10){
+
+            jimmySourceDown.volume = 1;
+            jimmySourceDown.Play();
+            biteCount++;
+            jimmyRain.Play();
+            return;
+        }
+        else{
+
+            audioSourceDown.clip = waffleBiteSounds[3];
             biteCount++;
 
         }
